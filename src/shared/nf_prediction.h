747ff533641115e87ad49fb8afee854855d0eca0
loydwilliams2015-droid <loydwilliams2015@gmail.com>
2026-08-07 21:24:20 -0500

    Carry combat intent through prediction history

--- src/shared/nf_prediction.h
+++ src/shared/nf_prediction.h
@@ -18,7 +18,7 @@ typedef struct NfPrediction {
 } NfPrediction;
 
 void nf_prediction_init(NfPrediction *prediction);
-NfInputCommand nf_prediction_make_command(NfPrediction *prediction, uint64_t client_tick, NfMoveInput move);
+NfInputCommand nf_prediction_make_command(NfPrediction *prediction, uint64_t client_tick, NfMoveInput move, NfCombatInput combat);
 void nf_prediction_record(NfPrediction *prediction, const NfInputCommand *command, const NfActor *actor);
 uint8_t nf_prediction_build_redundant_bundle(const NfPrediction *prediction, uint32_t newest_sequence, NfInputBundle *bundle);
 bool nf_prediction_reconcile(NfPrediction *prediction, NfWorld *world, NfEntityId entity_id, const NfActorNetState *authoritative, uint32_t acknowledged_input, uint64_t server_tick, float correction_threshold);
